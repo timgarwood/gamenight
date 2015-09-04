4d7aa1770b1954e585f5d93827ffe35dd867ae03
Tim Garwood <timgarwood@gmail.com>
2015-09-04 16:47:20 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var sqlite = require('sqlite3');
+var app = express();
+
+//server client side web pages statically
+app.use(express.static('./static'));
+
+app.post('/adduser', function(req, resp) {
+	console.log("add user requested");
+});
+
+app.post('/deleteuser', function(req, resp) {
+
+});
+
+app.post('/edituser', function(req, resp) {
+
+});
+
+app.get('/getuser', function(req, resp)) {
+}
+
+app.get('getusers', function(req, resp) {
+
+});
+
+app.listen(80);
